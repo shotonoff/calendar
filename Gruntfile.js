e1d1a0928d3aef467853f2a2cd44c3fc3337ddf8
golubev <golubev@coccoc.om>
2017-04-24 12:52:02 +0700

    Add root files

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        bowercopy: {
+            options: {
+                srcPrefix: 'bower_components',
+                destPrefix: 'public/assets'
+            },
+            scripts: {
+                files: {
+                    'js/jquery.js': 'jquery/dist/jquery.js',
+                    'js/bootstrap.js': 'bootstrap/dist/js/bootstrap.js',
+                    'js/angular.js': 'angular/angular.min.js',
+                    'js/angular-route.js': 'angular-route/angular-route.js',
+                    'js/jquery.validate.js': 'jquery-validation/dist/jquery.validate.js',
+                    'js/additional-methods.js': 'jquery-validation/dist/additional-methods.js',
+                    'js/jquery.validate.unobtrusive.js': 'jquery-validation-unobtrusive/jquery.validate.unobtrusive.js',
+                    'js/angular-jwt.js': 'angular-jwt/dist/angular-jwt.min.js',
+                    'js/moment.js': 'moment/min/moment.min.js',
+                    'js/angular-ui-calendar.js': 'angular-ui-calendar/src/calendar.js',
+                    'js/fullcalendar.js': 'fullcalendar/dist/fullcalendar.js',
+                    'js/fullcalendar-gcal.js': 'fullcalendar/dist/gcal.js',
+                    'js/bootstrap-datetimepicker.js': 'eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js',
+                    'js/spectrum.js': 'spectrum/spectrum.js',
+                }
+            },
+            stylesheets: {
+                files: {
+                    'css/bootstrap.css': 'bootstrap/dist/css/bootstrap.css',
+                    'css/bootstrap-theme.css': 'bootstrap/dist/css/bootstrap-theme.css',
+                    'css/fullcalendar.css': 'fullcalendar/dist/fullcalendar.css',
+                    'css/bootstrap-datetimepicker.css': 'eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.css',
+                    'css/spectrum.css': 'spectrum/spectrum.css',
+                    'css/sp-dark.css': 'spectrum/themes/sp-dark.css',
+                }
+            },
+            fonts: {
+                files: {
+                    'fonts': 'bootstrap/fonts'
+                }
+            }
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-bowercopy');
+    grunt.registerTask('install', ['bowercopy']);
+};
---
+++ README.md
@@ -0,0 +1,74 @@
+# Calendar events Application
+
+This application demonstrate my skills. I tried to cover full stack technologies 
+* PHP as a backend of app
+* Javascript as a frontend of app
+
+## Install the Application
+
+1. Clone the project [repository](https://git.itim.vn/shotonoff/aulinks)
+2. Copy `.env.example` to `.env`
+3. [Install docker](https://docs.docker.com/engine/installation/)
+4. [Install docker compose](https://docs.docker.com/compose/install/)
+
+5. If you are on OSX:
+
+   1. Add network interface for debug
+   
+   ```
+   sudo ifconfig lo0 alias 10.254.254.254
+   ```
+
+   2. Update `.env`
+   
+   ```bash
+   XDEBUG_REMOTE_HOST="10.254.254.254"
+   ```
+
+6. Add hostname to `/etc/hosts`:
+
+   ```bash
+   echo "\n127.0.0.1 aulinks.local" | sudo tee --append /etc/hosts
+   ```
+   
+7. Create docker network:
+    
+    ```bash
+    docker network create --driver bridge aulinks
+    ```
+
+8. Install and build project docker images:
+
+    ```bash
+    docker pull digitallyseamless/nodejs-bower-grunt:latest
+    docker-compose pull mysql nginx
+    docker-compose build php
+    docker-compose build nginx
+    ```
+
+9. Start project:
+    
+    Copy .env.example to .env
+    
+    ```bash
+        docker-compose up -d
+    ```
+
+10. Install project dependencies:
+
+    ```bash
+    bin/run npm install
+    bin/run bower install
+    bin/run bower bowercopy
+    composer install #is expected that composer already have been installed on your system
+    ```
+
+11. Prepare data: 
+    
+    ```bash
+        bin/run migration migrations:migrate --no-interaction
+        bin/run console user:create admin admin@aulinks.cz --super
+    ```
+    
+    
+
