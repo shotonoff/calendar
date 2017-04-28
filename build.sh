3b1a8c34d4208caac15e9ca76258a21576226631
golubev <golubev@coccoc.om>
2017-04-28 09:45:17 +0700

    Remove tabs into the file

--- build.sh
+++ build.sh
@@ -14,18 +14,18 @@
         docker network create --driver bridge aulinks
     fi
 
-	docker pull digitallyseamless/nodejs-bower-grunt:latest
-	docker-compose pull mysql nginx
-	docker-compose build php
-	bin/run npm install
-	bin/run bower install
-	bin/run grunt bowercopy
-	bin/run composer install
+    docker pull digitallyseamless/nodejs-bower-grunt:latest
+    docker-compose pull mysql nginx
+    docker-compose build php
+    bin/run npm install
+    bin/run bower install
+    bin/run grunt bowercopy
+    bin/run composer install
 
-	docker-compose up -d
+    docker-compose up -d
 
-	sleep 5
+    sleep 5
 
     docker-compose exec mysql mysql -uroot -proot -e "drop database if exists aulinks_db; create database aulinks_db;"
     docker-compose exec php ./vendor/bin/doctrine-migrations migrations:migrate --no-interaction
-	docker-compose exec php ./bin/console user:create admin admin@aulinks.cz --super
+    docker-compose exec php ./bin/console user:create admin admin@aulinks.cz --super
